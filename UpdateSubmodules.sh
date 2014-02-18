f4fcb869a5002d6ffe19f0a485ca2585bf17b071
Pierre-Olivier Latour <pol@mac.com>
2014-02-18 14:04:57 -0800

    Fix

--- UpdateSubmodules.sh
+++ UpdateSubmodules.sh
@@ -4,8 +4,10 @@ for DIRECTORY in *; do
   if [ -d "$DIRECTORY" ]; then
     if [ -e "$DIRECTORY/.git" ]; then
       echo "Updating $DIRECTORY..."
+      pushd "$DIRECTORY"
       git fetch
       git pull
+      popd
     fi
   fi
 done
